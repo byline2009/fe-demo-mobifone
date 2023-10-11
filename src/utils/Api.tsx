0a249ab03396ea1489e663c48f5009cf9fd208fd
unknown <mobifone@DESKTOP-S59VM5C.mobifone.vn>
2023-10-11 12:10:11 +0700

    update layout

--- src/utils/Api.tsx
+++ src/utils/Api.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_URL = process.env.REACT_APP_API_URL;
 const API = axios.create({
   baseURL: `${API_URL}`,
-  timeout: 10000,
+  timeout: 50000,
   headers: {
     "Content-Type": "application/json",
   },
