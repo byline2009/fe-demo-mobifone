d0d513b8ffc493c635150d159f212f226a919ab9
unknown <mobifone@DESKTOP-S59VM5C.mobifone.vn>
2024-01-22 08:42:31 +0700

    update layout

--- src/setup/axios/Axios.ts
+++ src/setup/axios/Axios.ts
@@ -18,18 +18,18 @@ export default function setupAxios(axios: any, store: any) {
   axios.interceptors.response.use(
     function (response: any) {
       // Do something with response data
-      //  console.log("response", response.data.result);
+      console.log("response", response.data.status);
 
       if (response.status == 200) {
         return response;
-      } else if (response.data.code == 401) {
+      } else if (response.data.code == 401 || response.data.code == 403) {
         store.dispatch({ type: actionTypes.Logout });
       }
     },
     function (error: any) {
       console.log(error.response);
       // Do something with response error
-      if (error.response.status == 401) {
+      if (error.response.status == 401 || error.response.status == 403) {
         store.dispatch({ type: actionTypes.Logout });
       } else if (error.response.status == 400) {
         console.log(error.response);
